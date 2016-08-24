502d7ecbb87f48b9f3e4b1a1b0bbfd2c72c630ac
kbolt <photonman_1@yahoo.com>
2016-08-24 10:39:29 -0600

    first commit

---
+++ js/navscroll.js
@@ -0,0 +1,38 @@
+
+// jQuery to collapse the navbar on scroll
+
+$(window).scroll(function() {
+    if ($(".navbar").offset().top > 50) {
+        $(".navbar-fixed-top").addClass("top-nav-collapse");
+    } else {
+        $(".navbar-fixed-top").removeClass("top-nav-collapse");
+    }
+});
+
+// jQuery for page scrolling feature - requires jQuery Easing plugin
+
+$(function() {
+    $('.page-scroll a').bind('click', function(event) {
+        var $anchor = $(this);
+        $('html, body').stop().animate({
+            scrollTop: $($anchor.attr('href')).offset().top
+        }, 800, 'easeInOutExpo');
+        event.preventDefault();
+    });
+});
+
+// owlCarousel
+$(document).ready(function() {
+ 
+  $("#owl-example").owlCarousel();
+ 
+});
+
+$("#owl-example").owlCarousel({ 
+        items:10,   
+        itemsDesktop : [1199,10],
+        itemsDesktopSmall : [980,9],
+        itemsTablet: [768,5],
+        itemsTabletSmall: false,
+        itemsMobile : [479,4]
+})
